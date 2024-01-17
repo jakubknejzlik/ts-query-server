e6f4fd0079510a3de7db94097d6e72038e7194e2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-01-17 15:52:10 +0100

    Fix timestream client export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { TimestreamQueryClient } from "@aws-sdk/client-timestream-query";
+export { QueryRouterClientAWSTimestream } from "./client-timestream";
 export { QueryRouterClientMySQL } from "./client-mysql";
 export { QueryRouter } from "./query-router";
